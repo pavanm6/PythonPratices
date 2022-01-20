29bad5b99e3bd6fefe6ae5fc424393a372f50539
pavanm6 <96732089+pavanm6@users.noreply.github.com>
2022-01-20 10:23:52 +0530

    Add files via upload

---
+++ Sample13.py
@@ -0,0 +1,18 @@
+# 14. WAP to accept project marks, external marks, internal marks from student,
+#  --> check if student got pass in project, internal and externals. 
+#    Note: pass mark is : 50
+#   --> if student got pass in project, internal and external then find totalscore.
+#    total_score= 70% from project+ 20% from external + 10% internal 
+#
+#  --> print grades based on total_score
+#          
+#        total_score >=90    --> A grade
+#	         	75--90       --> B grade
+#                50- 75      --> C grade
+#
+#   ---> print failed subject name along with the score.
+					
+					
+					
+
+project =int(input(""))
